8b9cfe5bc7f8a9d3307bbd41760bf585fafd8253
Vivek Kumar <97639152+Vivekkr7k@users.noreply.github.com>
2025-09-10 07:08:28 +0530

    Update firebase.js

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBS-lX2FeFKIPHy8v4NExt4u5CuWBeXvFw",
-  authDomain: "cyborgweb-32ba7.firebaseapp.com",
+  apiKey: "AIzaSyBS-lXNExt4u5CuWBeXvFw",
+  authDomain: "cyborgweb-32baseapp.com",
   projectId: "cyborgweb-32ba7",
-  storageBucket: "cyborgweb-32ba7.firebasestorage.app",
-  messagingSenderId: "630229978195",
-  appId: "1:630229978195:web:ab421d90cf2eb8aedf8aa6",
-  measurementId: "G-K7L9XXS8SL"
+  storageBucket: "cyborgwasestorage.app",
+  messagingSenderId: "630295",
+  appId: "1:630229978195:web:1d90cf2eb8aedf8aa6",
+  measurementId: "G7L9XXS8SL"
 };
 
 // Initialize Firebase
